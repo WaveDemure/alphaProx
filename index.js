26eb48e69ece3eb865047af9e3376c065b925156
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-21 21:55:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,73 +2,35 @@
 /**
  * @type {HTMLFormElement}
  */
-const form = document.getElementById("form");
+const form = document.getElementById("uv-form");
 /**
  * @type {HTMLInputElement}
  */
-const address = document.getElementById("address");
+const address = document.getElementById("uv-address");
 /**
  * @type {HTMLInputElement}
  */
-const searchEngine = document.getElementById("search-engine");
+const searchEngine = document.getElementById("uv-search-engine");
 /**
  * @type {HTMLParagraphElement}
  */
-const error = document.getElementById("error");
+const error = document.getElementById("uv-error");
 /**
  * @type {HTMLPreElement}
  */
-const errorCode = document.getElementById("error-code");
-
-const Cusprotocol = "proxy";
-
-function fetchCustomProtocol() {
-    return Cusprotocol
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    function openExec() {
-        var idn = prompt("input code");
-        if (!idn.includes("resetSettings")) {
-            try {
-                eval(idn);
-            } catch (error) {
-                alert(error)
-            }
-        } else {
-            resetSettings()
-        }
-    }
-
-    var msg = "executor";
-    var idx = 0
-    document.addEventListener("keydown", function (e) {
-        if (msg.length != idx) {
-            if (e.key.toLowerCase() === msg.charAt(idx).toLowerCase()) {
-                idx++;
-                console.log(`Correct key pressed: ${e.key}`);
-                if (msg.length === idx) {
-                    openExec()
-                }
-            }
-            console.log(`Current index: ${idx}`); 
-        }
-    });
-});
+const errorCode = document.getElementById("uv-error-code");
 
 form.addEventListener("submit", async (event) => {
-    event.preventDefault();
-  
-    try {
-      await registerSW();
-    } catch (err) {
-      error.textContent = "Failed to register service worker.";
-      errorCode.textContent = err.toString();
-      throw err;
-    }
-  
+  event.preventDefault();
 
+  try {
+    await registerSW();
+  } catch (err) {
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
 
-    const url = search(address.value, searchEngine.value);
-    location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
