4917c55a9d5f918cf358deca9962c79d9dd88629
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-23 14:36:07 -0600

    Temp changed the bare

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/a/',
-    bare: 'https://bare.benrogo.net',
+    bare: 'https://snorlaxscave.site/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
