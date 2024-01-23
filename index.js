f15922f781bd6896e489e86e445ba39d128780bc
Wave Demure <144980384+WaveDemure@users.noreply.github.com>
2024-01-22 18:14:21 -0800

    l

--- index.js
+++ index.js
@@ -11,26 +11,23 @@ const address = document.getElementById("address");
  * @type {HTMLInputElement}
  */
 const searchEngine = document.getElementById("search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
-const error = document.getElementById("error");
-/**
- * @type {HTMLPreElement}
- */
-const errorCode = document.getElementById("error-code");
 
 form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+    event.preventDefault()
+    var minEng = address.value.replace("https://www.google.com/search?q=%s", '')
+    console.log('address.value:', address.value);
+    console.log('minEng:', minEng);
+    if (address.value.includes(__protocoller$config.protocol+__protocoller$config.suffix)) {
+        test()
+    } else {
+        try {
+            await registerSW();
+        } catch (err) {
+            alert("Failed to register service worker.\n"+err.toString()+"|<button onclick=\"location.reload()\">return</button>")
+            throw err;
+        }
 
-  try {
-    await registerSW();
-  } catch (err) {
-    error.textContent = "Failed to register service worker.";
-    errorCode.textContent = err.toString();
-    throw err;
-  }
-
-  const url = search(address.value, searchEngine.value);
-  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        const url = search(address.value, searchEngine.value);
+        location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    }
 });
